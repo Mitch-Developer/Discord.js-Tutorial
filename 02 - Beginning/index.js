7fa3611b8b8f8375db02c730e49e770dcbe5370c
Mitch-Developer <114313832+Mitch-Developer@users.noreply.github.com>
2022-09-25 10:33:06 +0200

    Add files via upload

---
+++ 02 - Beginning/index.js
@@ -0,0 +1,11 @@
+const { Client, MessageEmbed } = require("discord.js");
+const config = require("./config.json");
+const token = config.token;
+
+const client = new Client({
+intents: 32767
+});
+module.exports = client;
+
+require("./events/ready.js")
+client.login(token); 
---
+++ 02 - Beginning/readme.md
@@ -0,0 +1,3 @@
+02 - Beginning 
+
+heyyy welcome to the next tutorial in this one i made the index for you guys!
